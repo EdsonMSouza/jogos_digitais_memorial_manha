b465235c1eba361426bc068a4edd72af50561f2f
EdsonMSouza <prof.edson.melo@gmail.com>
2020-04-07 11:44:17 -0300

    Site e API para os Placares

---
+++ site_api/api/class/UserModel.php
@@ -0,0 +1,90 @@
+<?php
+
+/**
+ * Model para manipulação de dados dos usuários
+ * 
+ * @author Edson Melo de Souza
+ * @date 2020-03
+ * 
+ */
+
+# inclusão da classe de conexão com o banco de dados
+include('PDOConnection.php');
+
+
+class UserModel
+{
+    private static $pdo;
+
+    /**
+     * Método Construtor da classe UserModel
+     */
+    function __construct()
+    {
+        self::$pdo = \PDOConnection::connection();
+    }
+
+    /**
+     * Insere um novo usuário
+     * Uma ideia de implementação seria criptografar a senha
+     */
+    public function new($name, $user, $password)
+    {
+        try {
+            # variável para armazenar a String SQL
+            $sql = "INSERT INTO users (name, user, password) VALUES (:name, :user, :password)";
+
+            # variável para armazenar o objeto da conexão que será utilizado para executar as operações
+            $stmt = self::$pdo->prepare($sql);
+
+            # atribuição dos valores informados para os parâmetros SQL
+            $stmt->bindValue(":name", $name, \PDO::PARAM_STR);
+            $stmt->bindValue(":user", $user, \PDO::PARAM_STR);
+            $stmt->bindValue(":password", $password, \PDO::PARAM_STR);
+
+            # executa a pesquisa no banco de dados
+            $stmt->execute();
+
+            return true;
+        } catch (PDOException $ex) {
+            # gera uma exceção (Exception) se ocorreu algum erro na interação com o banco de dados
+            throw $ex;
+        }
+    }
+
+    /**
+     * Métodos para manipulação dos dados de login
+     * Uma ideia de implementação seria criptografar a senha
+     */
+    public function login($user, $password)
+    {
+        try {
+            # variável para armazenar a String SQL
+            $sql = "SELECT id, name FROM users WHERE user = :user AND password = :password";
+
+            # variável para armazenar o objeto da conexão que será utilizado para executar as operações
+            $stmt = self::$pdo->prepare($sql);
+
+            # atribuição dos valores informados para os parâmetros SQL
+            $stmt->bindValue(":user", $user, \PDO::PARAM_STR);
+            $stmt->bindValue(":password", $password, \PDO::PARAM_STR);
+
+            # executa a pesquisa no banco de dados
+            $stmt->execute();
+
+            # retorna um objeto - o tratamento deve ser feito no Controller para verificar se há dados retornados
+            return $stmt->fetch(\PDO::FETCH_OBJ);
+        } catch (PDOException $ex) {
+            # gera uma exceção (Exception) se ocorreu algum erro na interação com o banco de dados
+            throw $ex;
+        }
+    }
+
+    /**
+     * Métodos para manipulação dos dados de login
+     * Uma ideia de implementação seria criptografar a senha
+     */
+    public function users()
+    {
+    }
+}
---
+++ site_api/index.php
@@ -0,0 +1,11 @@
+<html>
+
+<head>
+    <title>Placar Online - Jogos Digitais</title>
+</head>
+
+<body>
+    <h1>Placar Online - Jogos Digitais</h1>
+</body>
+
+</html>
