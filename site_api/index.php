4f7ca80f6030645b1bcc01302c63cbff62c11949
EdsonMSouza <prof.edson.melo@gmail.com>
2020-04-16 11:58:08 -0300

    Página que mostra o Placar Online

--- site_api/index.php
+++ site_api/index.php
@@ -1,11 +1,67 @@
+<?php
+
+# envia os dados para API
+$array = array("type" => "ranking_general");
+$json = json_encode($array);
+$ch = curl_init('colocar a URL da API aqui');
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Content-Length: ' . strlen($json)));
+
+# recupera os dados retornados da API
+$obj = json_decode(curl_exec($ch));
+
+# verifica se ocorreu algum erro na requisição
+if ($obj === null && json_last_error() !== JSON_ERROR_NONE) {
+    echo json_encode(["Mensagem:" => "Incorrect Request"]);
+    die();
+}
+?>
+
+<!DOCTYPE html>
 <html>
 
 <head>
-    <title>Placar Online - Jogos Digitais</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Ranking Geral</title>
+
+    <!-- Importa o Framework Bootstrap para o arquivo -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css">
 </head>
 
 <body>
-    <h1>Placar Online - Jogos Digitais</h1>
+    <!-- Container principal da aplicação -->
+    <div class="container">
+        <!-- Aqui vai o resultado da consulta -->
+        <div class="col-md-8" style="margin: auto;">
+            <br>
+            <h1 class="text-center">Ranking Geral dos Jogos</h1>
+            <br>
+            <table class="table">
+                <thead class="thead-dark">
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Usuário</th>
+                        <th class="text-right" scope="col">Score</th>
+                    </tr>
+                </thead>
+
+                <?php
+                # percore o objeto JSON e mostra os dados
+                $contador = 1;
+                foreach ($obj as $scores) {
+                    foreach ($scores as $values) {
+                        print("<tr>");
+                        print("<td>" . $contador++ . "</td>");
+                        print("<td>" . $values->User . "</td>");
+                        print("<td class=\"text-right\">" . $values->Score . "</td>");
+                    }
+                }
+                ?>
+            </table>
+        </div>
+    </div>
 </body>
 
 </html>
